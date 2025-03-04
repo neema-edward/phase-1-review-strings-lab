33d8e1bf8a42c4c8dd41edc1f77cfba5207eef08
Neema <neema.edward@student.moringaschool.com>
2025-03-04 11:05:06 +0300

    adds const variables

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-let welcomeMessage = "Welcome to Flatbook!"// Write your code in this file!
-let currentUser = "Neema Edward"
+const currentUser = 'Neema Edward'
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`
+
+
